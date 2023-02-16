6c5b2726048a88aaf024c33283b76a71acc0f7d7
Hanji <kaitothedraggy@gmail.com>
2023-02-16 15:59:28 +0100

    noise value from 0 to 1

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -1,38 +1,78 @@
 extends Node3D
 
+enum Block {
+	Grass = 0,
+	Dirt,
+	Stone,
+	StoneGrass,
+	Sand
+}
+
 func _input(event):
 	if event.is_action_pressed("Interact"):
 		$Wolf/SubViewport/Node2D.calculate_hue()
 		random_terrain()
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
 
 func _ready():
 	random_terrain()
 
 func random_terrain():
 	$GridMap.clear()
-	
-	var noise = FastNoiseLite.new()
-	noise.seed = randi()
-	noise.fractal_gain = 6
-	noise.fractal_octaves = 8
-	noise.fractal_lacunarity = 1.25
+	var noise = $GridMap.get_meta("NoiseColorMap").noise
 	
 	var count : int = 0
 	var starttime = Time.get_ticks_msec()
 	
-	for i in range(-256,256):
-		for j in range(-256,256):
-			var greyvalue : float = noise.get_noise_2d(i,j)
-			var id : int = 0
-			var y : int = 0
+	var size: int = 128
+	var noiseMap = []
+	
+	var maxNoiseHeight:float = -1
+	var minNoiseHeight:float = 1
+	
+	for i in range(size):
+		noiseMap.append([])
+		#noiseMap.resize(size)
+		for j in range(size):
+			var noiseHeight: float = noise.get_noise_2d(i,j)
+			if noiseHeight > maxNoiseHeight:
+				maxNoiseHeight = noiseHeight
+			elif noiseHeight < minNoiseHeight:
+				minNoiseHeight = noiseHeight
 			
-			if greyvalue < 0.25 : id = 0
-			elif greyvalue >= 0.25 and greyvalue < 0.5: id = 1 ; y = 1
-			elif greyvalue >= 0.5 and greyvalue < 0.75: id = 2; y = 2
-			elif greyvalue >= 0.75 and greyvalue < 0.95: id = 3 ; y = 3
-			elif greyvalue >= 0.95 : id = 4
-			$GridMap.set_cell_item(Vector3i(i,y,j), id)
-			count += 1
+			noiseMap[i].append(noiseHeight)
+			
+	for x in range(size):
+		for y in range(size):
+			noiseMap[x][y] = inverse_lerp(minNoiseHeight, maxNoiseHeight, noiseMap[x][y])
+
+			var id: int = 0
+			var height: int = 0
+			var noiseHeight = noiseMap[x][y]
+			if noiseHeight < 0.4 : id = Block.Sand
+			elif noiseHeight >= 0.4 and noiseHeight < 0.75: 
+				id = Block.Grass ; height = 1
+				$GridMap.set_cell_item(Vector3i(x,0,y), Block.Sand)
+			elif noiseHeight >= 0.75 and noiseHeight < 0.9: 
+				id = Block.Stone; height = 2
+				$GridMap.set_cell_item(Vector3i(x,0,y), Block.Sand)
+				$GridMap.set_cell_item(Vector3i(x,1,y), Block.Grass)
+			
+			elif noiseHeight >= 0.9: id = Block.StoneGrass ; height = 3
+			$GridMap.set_cell_item(Vector3i(x,height,y), id)
+	
+	var cells = $GridMap.get_used_cells()
+	for cellCoord in cells:
+		if cellCoord.y == 1:
+			for i in range(cellCoord.x - 1, cellCoord.x + 2):
+				for j in range(cellCoord.z - 1, cellCoord.z + 2):
+					var neighborCell = Vector3i(i, cellCoord.y, j)
+					if $GridMap.get_cell_item(neighborCell) == $GridMap.INVALID_CELL_ITEM:
+						$GridMap.set_cell_item(cellCoord, Block.Sand)
+						break
 	
 	var endtime = Time.get_ticks_msec() - starttime
 	print("Placed " + str(count) + " blocs in " + str(endtime) + "ms")
+	
+	
