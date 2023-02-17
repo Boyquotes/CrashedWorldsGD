43d56125470c43ddaa43c59a1c4a3d5b64478ff3
Sacha-Zaffran <sacha.zaffran@gmail.com>
2023-02-17 12:17:13 +0100

    Idle Fix

--- Entities/Wolf/Wolf3D/wolf3D.gd
+++ Entities/Wolf/Wolf3D/wolf3D.gd
@@ -4,7 +4,7 @@ var Target : Player
 var speed = 3
 var guardSpeed = 1
 var seePlayer
-var target_pos : Vector3
+var target_pos : Vector3 = Vector3(0,1,0)
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 2
 #var xlocation = randf_range(-360, 360)
 #var zlocation = randf_range(-360, 360)
@@ -26,39 +26,36 @@ enum states {IDLE, GUARD, AGGRO, ATTACK, BITE}
 #----------------------------------------------------------- BASE
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target_pos = transform.origin
+	randomize()
 	_on_state_changed(State)
 
 func _physics_process(delta) -> void:
 	# Raycast vision of the AI
-	if Target != null:
-		$RayCast3D.target_position = Target.global_position - $RayCast3D.global_position
-		if $RayCast3D.is_colliding() and $RayCast3D.get_collider() is Player:
-			seePlayer = true
-		else:
-			seePlayer = false
-	# Switch states
-		if seePlayer == true:
-			match State :
-				states.IDLE : _Idle()
-				states.GUARD : _Guard()
-				states.AGGRO : _Aggro()
-				states.ATTACK : _Attack()
-				states.BITE : _Bite()
-			if not is_on_floor():
-				velocity.y -= gravity * delta
-
-	velocity.y -= 9.8
-	if velocity.x > 0:
-		$SubViewport/Node2D.flip(true)
+	$RayCast3D.target_position = target_pos - $RayCast3D.global_position
+	if $RayCast3D.is_colliding() and $RayCast3D.get_collider() is Player:
+		seePlayer = true
 	else:
-		$SubViewport/Node2D.flip(false)
+		seePlayer = false
+	# Switch states
+	match State :
+		states.IDLE : _Idle()
+		states.GUARD : _Guard()
+		states.AGGRO : _Aggro()
+		states.ATTACK : _Attack()
+		states.BITE : _Bite()
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+		if velocity.x > 0:
+			$SubViewport/Node2D.flip(true)
+		else:
+			$SubViewport/Node2D.flip(false)
 	move_and_slide()
 
 #----------------------------------------------------------- CUSTOM
 func _Idle():
+	
 	$Label3D.text = "IDLE"
-	Target = null
 #	_on_timer_timeout()
 	if $Timer.is_stopped():
 		$Timer.wait_time = randf_range(1.0, 5.0)
@@ -68,24 +65,29 @@ func _Idle():
 			target_pos = Vector3(randf_range(-3.0, 3.0), 0.0, randf_range(-5.0, 5.0))
 			target_pos += transform.origin
 		else:
-			return
+			pass
 	
 	if target_pos.distance_to(transform.origin) > 0.1:
 		velocity = target_pos - transform.origin # distance
 		velocity = velocity.normalized() * guardSpeed # vector towards the pos
+		
+		
 		#INSERT ANIMATIONS
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
-		#INSERT ANIMATIONS
+		print("TRUE")
+		#INSERT ANIMATION
+	velocity.y = 0
 
 func _Guard():
 	$Label3D.text = "GUARD"
-	if Target :
+	if Target and seePlayer:
 		var movement = (Target.global_position -  global_position).normalized() * guardSpeed
 		velocity = movement
 		velocity.y = 0.0
 		#INSERT ANIMATIONS
+	
 
 func _Aggro():
 	$Label3D.text = "AGGRO"
