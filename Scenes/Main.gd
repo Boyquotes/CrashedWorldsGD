ff718209a216c67fc4fa820c72fef8426b2f413b
FaerDerr <matheo.souville@gmail.com>
2023-02-19 21:43:58 +0100

    Last commit of February's week

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -23,12 +23,13 @@ func _ready():
 
 func random_terrain():
 	$GridMap.clear()
+	$GridMap.set_meta("NoiseColorMap.seed", randi())
 	var noise = $GridMap.get_meta("NoiseColorMap").noise
 	
 	var count : int = 0
 	var starttime = Time.get_ticks_msec()
 	
-	var size: int = 128
+	var size: int = 256
 	var noiseMap = []
 	
 	var maxNoiseHeight:float = -1
@@ -99,10 +100,24 @@ func destroyGrid(pos: Vector3i):
 		Block.Sand: blocitem = load("res://Resources/Item/Blocs/SandBloc.tres")
 		Block.Berries: blocitem = load("res://Resources/Item/Lootables/Berries/RedBerry.tres")
 	
-	inst.item = blocitem
-	$Items.add_child(inst)
-	inst.global_position = Vector3(pos.x + 0.5, pos.y + 0.5, pos.z+0.5)
-	$GridMap.set_cell_item(pos, $GridMap.INVALID_CELL_ITEM)
+	var blocID = $GridMap.get_cell_item(pos)
+	var blocitem = []
+	match blocID:
+		0: blocitem.appends(load("res://Resources/Item/Blocs/GrassBloc.tres"))
+		1: blocitem.append(load("res://Resources/Item/Blocs/DirtBloc.tres"))
+		2: blocitem.append(load("res://Resources/Item/Blocs/StoneBloc.tres"))
+		3: blocitem.append(load("res://Resources/Item/Blocs/GrassyStoneBloc.tres"))
+		4: blocitem.append(load("res://Resources/Item/Blocs/SandBloc.tres"))
+		5: 
+			blocitem.append(load("res://Resources/Item/Lootables/Berries/RedBerry.tres"))
+			blocitem.append(load("res://Resources/Item/Lootables/Wood.tres"))
+	
+	for i in blocitem:
+		var inst = itemdrop.instantiate()
+		inst.item = i
+		$Items.add_child(inst)
+		inst.global_position = Vector3(pos.x + 0.5, pos.y + 0.5, pos.z+0.5)
+		$GridMap.set_cell_item(pos, $GridMap.INVALID_CELL_ITEM)
 
 func placeGrid(pos: Vector3i, id:int):
 	if $GridMap.get_cell_item(pos) == $GridMap.INVALID_CELL_ITEM:
